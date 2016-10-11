c290deda20818ea0e01da745c747012261f881de
Shingo Hiraya <shingohiraya@gmail.com>
2016-10-11 10:54:27 +0900

    first commit

---
+++ LivePhotoEditor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LivePhotoEditor
+//
+//  Created by hiraya.shingo on 2016/10/11.
+//  Copyright © 2016年 Shingo Hiraya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
